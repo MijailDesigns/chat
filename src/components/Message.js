67a735d11dfd7736799747cb6ee8b9758dfb1e0b
mijail pulgar <mijail86@gmail.com>
2023-01-18 16:42:34 -0500

    emojis incluidos

--- src/components/Message.js
+++ src/components/Message.js
@@ -6,12 +6,23 @@ const Message = ({ message }) => {
 
   const [user] = useAuthState(auth);
 
+  const date = new Date(message.createdAt?.seconds*1000);
+    const options = { 
+        month: 'long', 
+        day: 'numeric' 
+    };
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let time = h + ":" + m;
+    const newDate = date.toLocaleDateString('en-US', options);
+
   return (
     <div className={`chat-bubble ${message.uid === user.uid ? "right" : ""}`}>
       <img className="chat-bubble__left" src={message.avatar} alt="user avatar" />
       <div className="chat-bubble__right">
         <p className="user-name">{message.name}</p>
         <p className="user-message">{message.text}</p>
+        <p className="user">{`${newDate} - ${time}`}</p>
       </div>
     </div>
   );
--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import { auth, db } from "../firebase";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import Picker from 'emoji-picker-react';
 
 const SendMessage = ({ scroll }) => {
 
   const [message, setMessage] = useState("");
+  const [open, setOpen] = useState('close');
 
   const sendMessage = async (event) => {
     event.preventDefault();
@@ -24,8 +26,36 @@ const SendMessage = ({ scroll }) => {
     scroll.current.scrollIntoView({ behavior: "smooth" });
   };
 
+  const emoji = () => {
+      setOpen('open');
+  }
+  const closeEmoji = () => {
+      setOpen('close');
+  }
+  const onEmojiClick = (event, emojiObject) => {
+    console.log(event.emoji)
+    setMessage(`${message}${event.emoji}`)
+  };
+
   return (
     <form onSubmit={(event) => sendMessage(event)} className="send-message">
+
+      <button 
+          type="button" 
+          className="btn-emoji"
+          onClick={emoji}>
+              😉
+      </button>
+      <div className={open}>
+          <button 
+              className="close-emoji" 
+              onClick={closeEmoji}
+              type="button">
+                  ❌
+          </button>
+          <Picker onEmojiClick={onEmojiClick}/>
+      </div>
+
       <label htmlFor="messageInput" hidden>
         Enter Message
       </label>
